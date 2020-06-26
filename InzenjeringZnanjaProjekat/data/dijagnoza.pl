f0c7c6c135a43cecf4624abfe98ce1f232c00feb
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-06-26 16:24:19 +0200

    Updejtovan dijagnoza.pl fajl

--- InzenjeringZnanjaProjekat/data/dijagnoza.pl
+++ InzenjeringZnanjaProjekat/data/dijagnoza.pl
@@ -1,14 +1,56 @@
-diagnosis_for_tests(essential_tremor, hematologic_tests).
-diagnosis_for_tests(essential_tremor, radiographic_imaging_procedure).
-diagnosis_for_tests(essential_tremor, complete_blood_count).
+diagnosis_for_tests(essential_tremor, magnetic_resonance_imaging).
+diagnosis_for_tests(essential_tremor, physical_therapy_exercises).
+diagnosis_for_tests(essential_tremor, ophthalmic_examination_and_evaluation).
 
-diagnosis_for_tests(parkinson_disease, radiographic_imaging_procedure).
-diagnosis_for_tests(parkinson_disease, complete_blood_count).
+diagnosis_for_tests(parkinson_disease, physical_therapy_exercises).
+diagnosis_for_tests(parkinson_disease, magnetic_resonance_imaging).
+diagnosis_for_tests(parkinson_disease, depression_screen).
 
-diagnosis_for_tests(restless_leg_syndrome, hematologic_tests).
+diagnosis_for_tests(restless_leg_syndrome, magnetic_resonance_imaging).
 diagnosis_for_tests(restless_leg_syndrome, complete_blood_count).
-diagnosis_for_tests(restless_leg_syndrome, x_ray).
+diagnosis_for_tests(restless_leg_syndrome, hematologic_tests).
 
 diagnosis_for_tests(alcohol_abuse, hematologic_tests).
 diagnosis_for_tests(alcohol_abuse, complete_blood_count).
-diagnosis_for_tests(alcohol_abuse, x_ray).
+diagnosis_for_tests(alcohol_abuse, mental_health_counseling).
+diagnosis_for_tests(alcohol_abuse, urinalysis).
+
+
+diagnosis_for_tests(anxiety, physical_therapy_exercises).
+diagnosis_for_tests(anxiety, depression_screen).
+diagnosis_for_tests(anxiety, glucose_measurement).
+
+
+diagnosis_for_tests(developmental_disability, mental_health_counseling).
+diagnosis_for_tests(developmental_disability, physical_therapy_exercises).
+diagnosis_for_tests(developmental_disability, magnetic_resonance_imaging).
+
+diagnosis_for_tests(migraine, intravenous_fluid_replacement).
+diagnosis_for_tests(migraine, glucose_measurement).
+diagnosis_for_tests(migraine, urinalysis).
+
+
+diagnosis_for_tests(common_cold, complete_blood_count).
+diagnosis_for_tests(common_cold, urinalysis).
+diagnosis_for_tests(common_cold, glucose_measurement).
+
+diagnosis_for_tests(chronic_sinusitis, x_ray).
+diagnosis_for_tests(chronic_sinusitis, kidney_function_tests).
+diagnosis_for_tests(chronic_sinusitis, urinalysis).
+
+
+diagnosis_for_tests(alzheimer_disease, electrocardiogram).
+diagnosis_for_tests(alzheimer_disease, mental_healt_counseling).
+diagnosis_for_tests(alzheimer_disease, physical_therapy_exercises).
+
+diagnosis_for_tests(delirium, hematologic_tests).
+diagnosis_for_tests(delirium, complete_blood_count).
+diagnosis_for_tests(delirium, glucose_measurement).
+
+diagnosis_for_tests(depression, mental_healt_counseling).
+diagnosis_for_tests(depression, depression_screen).
+diagnosis_for_tests(depression, physical_therapy_exercises).
+
+diagnosis_for_tests(dementia, electrocardiogram).
+diagnosis_for_tests(dementia, x_ray).
+diagnosis_for_tests(dementia, mental_healt_counseling).
